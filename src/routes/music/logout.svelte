919b8d00fa0ce7d29b31b0501da2207a1ef60566
Kapil <kapil.pipaliya@yahoo.com>
2019-09-04 06:56:02 +0530

    improved logout speed

--- src/routes/music/logout.svelte
+++ src/routes/music/logout.svelte
@@ -8,10 +8,25 @@ import { ws_madmin, isAuthFn } from "../_modules/functions.js";
 export async function preload(page, session) {
   const redirect_url = "music/login?message=Logout successfully...&type=info"
   let S; if (typeof S_ == "function") { S = new S_(ws_madmin, this.req, this.res); } else { S = S_; }
-  const isAuth = await new Promise((resolve, reject) => { S.bind_( ["user", `is_logged_in`, 0], ([d]) => { resolve(d); }, [[]] ); });
-  if(!isAuth){ this.redirect(302, redirect_url) }
   
-  const logout = await new Promise((resolve, reject) => { S.bind_( ["auth", "logout", 0], ([d]) => { resolve(d); }, [[]] ); });
+  const getTableData = async(filterSettings=[]) => {
+    let isAuth = false;
+    let logout = {}
+    await new Promise((resolve, reject) => {
+        const batch1 = []
+        let r00 = false, r0 = false
+        const myResolve = () => {
+          if(r00 && r0) resolve(1)
+        }
+        batch1.push([ ["user", `is_logged_in`, 0], ([d]) => {isAuth = d; r00 = true; myResolve()}, [[]] ])
+        batch1.push([ ["auth", "logout", 0], ([d]) => {logout = d; r0 = true; myResolve()}, [[]] ])
+        S.batchBind_T(batch1)
+      })
+    return {isAuth, logout};
+  }
+
+  const {isAuth, logout} = await getTableData([]);
+  if(!isAuth){ this.redirect(302, redirect_url) }
   if(logout.ok){ 
     document.cookie = `admin=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
     this.redirect(302, redirect_url) 
