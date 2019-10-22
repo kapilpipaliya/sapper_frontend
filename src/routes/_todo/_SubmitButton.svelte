90b4a3c3868e75195bf1ec6ab53cc79c7422a9ac
Kapil <kapil.pipaliya@yahoo.com>
2019-10-22 18:35:12 +0530

    removed todo app. finally delete this all repo.

--- src/routes/_todo/_SubmitButton.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  export let isSaving = false;
-  $: buttonSaveClass = isSaving === true ? "pure-button pure-button-disabled loading" : "pure-button success";
-</script>
-<style src="./_SubmitButton.scss"></style>
-<button type="submit" class={buttonSaveClass} disabled={isSaving}>Save changes</button>
